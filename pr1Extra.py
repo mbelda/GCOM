1ae36c88816ec744fda2ff17e1266d824650dfde
Maria José Belda <majo.belda15@gmail.com>
2020-02-14 11:01:14 +0100

    Update pr1Extra.py

--- pr1Extra.py
+++ pr1Extra.py
@@ -6,24 +6,24 @@ from PIL import Image
 iteracionesTotales = 7
 #Tamaño total de la imagen
 tam = 3**iteracionesTotales
-#Creamos el cuadrado grande vacío y lo pintamos blanco
-square = np.empty([tam, tam, 3], dtype = np.uint8)
-square.fill(0)
+#Creamos el cuadrado grande y lo pintamos negro
+cuadrado = np.empty([tam, tam, 3], dtype = np.uint8)
+cuadrado.fill(0)
 
-negro = np.array([255, 255, 255], dtype = np.uint8) 
+blanco = np.array([255, 255, 255], dtype = np.uint8) 
  
-for i in range(0, iteracionesTotales + 1): 
+for i in range(iteracionesTotales + 1): 
 	tamCuadradoi = 3**(iteracionesTotales - i) 
-	for x in range(0, 3**i): 
+	for x in range(3**i): 
 		#Busco el cuadrado central
 		if x % 3 == 1: 
-			for y in range(0, 3**i): 
+			for y in range(3**i): 
 				if y % 3 == 1: 
-                    #Lo pinto negro
-					square[x * tamCuadradoi : (x + 1) * tamCuadradoi,\
-                            y * tamCuadradoi : (y + 1) * tamCuadradoi] = negro 
+                    #Lo pinto blanco
+					cuadrado[x * tamCuadradoi : (x + 1) * tamCuadradoi,\
+                            y * tamCuadradoi : (y + 1) * tamCuadradoi] = blanco 
 
 #Guardar y abrir la imagen
-imgio.imwrite('sierpinski.jpg', square[:,:,0])
+imgio.imwrite('sierpinski.jpg', cuadrado[:,:,0])
 im = Image.open("sierpinski.jpg") 
 im.show()
