c38382b050657cec99778728edbd46edf5ce7711
Maria José Belda <majo.belda15@gmail.com>
2020-02-14 13:01:19 +0100

    P1 funcionando

--- pr1.1.py
+++ pr1.1.py
@@ -3,10 +3,16 @@ import matplotlib.pyplot as plt
 
 
 epsilon=0.001
-r=3.25
-x0=0.5
-delta_x0 = 0.01
+r = 3.4
+x0 = 0.2
+delta_x0 = 0.05
 delta_r = 0.001
+
+def resta(x,y):
+    res = []
+    for i in range(len(x)):
+        res.append(np.abs(x[i] - y[i]))
+    return res
 
 def logistica(x):
     return r*x*(1-x)
@@ -19,33 +25,111 @@ def orbita(x0,f,n):
         orbita.append(x)
     return orbita
 #arange(start,stop,step)
-for x0 in np.arange(0, 1 + delta_x0, delta_x0):
-    for r in np.arange(3 + delta_r, 3.5, delta_r):
-        M = 1000
-        N = 20
-        orb = orbita(x0, logistica, M)
-        posiblesk = [i for i in range(M-20, M)]
-        V0 = []
-        #Calcular tambien la cota de error de los vi del V0
-        #Quedarse con el noveno mas grande (el percentil 90)
-        #Como no es gaussiano hay que estimarlo
-        equivalentesk = []
-        for k in reversed(posiblesk):
-            if k not in equivalentesk :
-                kbueno = False
-                for i in reversed(posiblesk):
-                    if i != k and i not in equivalentesk and abs(orb[k] - orb[i]) < epsilon:
-                        kbueno = True
-                        equivalentesk.append(i)
-                if kbueno : V0.append(k)
-                equivalentesk.append(k)
-            
-        if V0 == [] : print('No hay ks')
+
+M = 100
+N = 20
+orb = orbita(x0, logistica, M)
+posiblesk = [i for i in range(M-20, M)]
+V0 = []
+V0orb = []
+#Calcular tambien la cota de error de los vi del V0
+#Quedarse con el noveno mas grande (el percentil 90)
+#Como no es gaussiano hay que estimarlo
+equivalentesk = []
+for k in reversed(posiblesk):
+    if k not in equivalentesk :
+        kbueno = False
+        for i in range(k):
+            if i not in equivalentesk and abs(orb[k] - orb[k-i-1]) < epsilon:
+                kbueno = True
+                equivalentesk.append(k-i-1)
+        if kbueno :
+            V0.append(k)
+            V0orb.append(orb[k])
+        equivalentesk.append(k)
+    
+if V0 == [] : print('No hay ks')
+errores = []
+V0orb.sort()   
+Vaux = V0orb
+for j in range(0, 10):
+    for i in range(len(V0)):
+        Vaux[i] = logistica(Vaux[i])
+    Vaux.sort()    
+    errores.append(max(resta(V0orb, Vaux)))
+
+errores.sort()  
+print(errores)
+print('El error es:', errores[8])
+print('V0: ', V0)
+
+
+for x0 in np.arange(x0, x0 + delta_x0, delta_x0/10):
+    M = 100
+    N = 20
+    orb = orbita(x0, logistica, M)
+    posiblesk = [i for i in range(M-20, M)]
+    V0aux = []
+    V0auxorb = []
+    #Calcular tambien la cota de error de los vi del V0
+    #Quedarse con el noveno mas grande (el percentil 90)
+    #Como no es gaussiano hay que estimarlo
+    equivalentesk = []
+    for k in reversed(posiblesk):
+        if k not in equivalentesk :
+            kbueno = False
+            for i in range(k):
+                if i not in equivalentesk and abs(orb[k] - orb[k-i-1]) < epsilon:
+                    kbueno = True
+                    equivalentesk.append(k-i-1)
+            if kbueno :
+                V0aux.append(k)
+                V0auxorb.append(orb[k])
+            equivalentesk.append(k)
+    
+    V0auxorb.sort()
+    print(V0orb)
+    print(V0auxorb)
+    if max(resta(V0orb,V0auxorb)) > epsilon:
+        print('No es estable')
+V0s = np.array([])
+#np.linspace(start, stop, numintervals)
+enc = False
+for r in np.arange(2.5 + delta_r, 4, delta_r):
+    M = 100
+    N = 20
+    orb = orbita(x0, logistica, M)
+    posiblesk = [i for i in range(M-20, M)]
+    V0aux = []
+    V0auxorb = []
+    #Calcular tambien la cota de error de los vi del V0
+    #Quedarse con el noveno mas grande (el percentil 90)
+    #Como no es gaussiano hay que estimarlo
+    equivalentesk = []
+    for k in reversed(posiblesk):
+        if k not in equivalentesk :
+            kbueno = False
+            for i in range(k):
+                if i not in equivalentesk and abs(orb[k] - orb[k-i-1]) < epsilon:
+                    kbueno = True
+                    equivalentesk.append(k-i-1)
+            if kbueno :
+                V0aux.append(k)
+                V0auxorb.append(orb[k])
+            equivalentesk.append(k)
+    
+    V0auxorb.sort()
+    V0auxorb = np.array(V0auxorb)
+    np.append(V0s, V0auxorb)
+    
+    for y in V0auxorb:
+        plt.plot(r, y, ',k', alpha=0.25)
         
-        if len(V0) > 4 : 
-            print('x0: ', x0, ' r: ', r)
-            print('V0: ', V0)
-            #Siempre sale V0 con 1, 2 o 4 elementos (1 cuando x0 = 0 o 1)
+    if len(V0aux) == 8 and enc == False:
+        enc = True
+        print('Los elementos son 8 a partir de r =', r)
+
+        
 plt.show()
 
 
--- pr1Extra.py
+++ pr1Extra.py
@@ -13,15 +13,25 @@ cuadrado.fill(0)
 blanco = np.array([255, 255, 255], dtype = np.uint8) 
  
 for i in range(iteracionesTotales + 1): 
-	tamCuadradoi = 3**(iteracionesTotales - i) 
-	for x in range(3**i): 
-		#Busco el cuadrado central
-		if x % 3 == 1: 
-			for y in range(3**i): 
-				if y % 3 == 1: 
+    tamCuadradoi = 3**(iteracionesTotales - i)
+    #Para cada iteración busco la medida de hausdorff
+    if i != 0:
+        Nr = 8**i
+        r = (1/3)**i
+        medida = np.abs(np.log(Nr)/np.log(r))
+        print('Medida it', i, ': ', medida)
+        
+    else:
+        print('Log8/log3 =', np.log(8)/np.log(3))
+        
+    for x in range(3**i): 
+        #Busco el cuadrado central
+        if x % 3 == 1: 
+            for y in range(3**i):
+                if y % 3 == 1: 
                     #Lo pinto blanco
-					cuadrado[x * tamCuadradoi : (x + 1) * tamCuadradoi,\
-                            y * tamCuadradoi : (y + 1) * tamCuadradoi] = blanco 
+                    cuadrado[x * tamCuadradoi : (x + 1) * tamCuadradoi,\
+                    y * tamCuadradoi : (y + 1) * tamCuadradoi] = blanco
 
 #Guardar y abrir la imagen
 imgio.imwrite('sierpinski.jpg', cuadrado[:,:,0])
