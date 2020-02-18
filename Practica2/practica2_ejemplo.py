52c1d3f801e844148152a8632bb7fbe560c43b83
Maria José Belda <majo.belda15@gmail.com>
2020-02-18 11:50:31 +0100

    p2 acabada

--- Practica2/practica2_ejemplo.py
+++ Practica2/practica2_ejemplo.py
@@ -5,6 +5,7 @@ Práctica 2
 import os
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 #### Carpeta donde se encuentran los archivos ####
 ubica = r"C:\Users\Majo\Documents\GitHub\GCOM\Practica2"
@@ -45,24 +46,6 @@ distr_es = pd.DataFrame({'states': tab_es_states, 'probab': tab_es_probab })
 distr_es = distr_es.sort_values(by='probab', ascending=True)
 distr_es.index=np.arange(0,len(tab_es_states))
 
-##### Para obtener una rama, fusionamos los dos states con menor frecuencia
-distr = distr_en
-''.join(distr['states'][[0,1]])
-
-### Es decir:
-states = np.array(distr['states'])
-probab = np.array(distr['probab'])
-state_new = np.array([''.join(states[[0,1]])])   #Ojo con: state_new.ndim
-probab_new = np.array([np.sum(probab[[0,1]])])   #Ojo con: probab_new.ndim
-codigo = np.array([{states[0]: 0, states[1]: 1}])
-states =  np.concatenate((states[np.arange(2,len(states))], state_new), axis=0)
-probab =  np.concatenate((probab[np.arange(2,len(probab))], probab_new), axis=0)
-distr = pd.DataFrame({'states': states, 'probab': probab, })
-distr = distr.sort_values(by='probab', ascending=True)
-distr.index=np.arange(0,len(states))
-
-#Creamos un diccionario
-branch = {'distr':distr, 'codigo':codigo}
 
 ## Ahora definimos una función que haga exáctamente lo mismo
 def huffman_branch(distr):
@@ -88,40 +71,101 @@ def huffman_tree(distr):
         tree = np.concatenate((tree, code), axis=None)
     return(tree)
  
-distr = distr_en 
-tree = huffman_tree(distr)
-print(tree)
-#print(tree[0].items())
-#print(tree[0].values())
 
-def codifletra(letra):
-    codigo = []
+tree_en = huffman_tree(distr_en)
+#print(tree_en)
+tree_es = huffman_tree(distr_es)
+#print(tree_es)
+
+def codifletra(letra, tree):
+    codigo = ''
     for nodo in np.flip(tree):
         keys = np.array(list(nodo.keys()))
         values = np.array(list(nodo.values()))
 
         if letra in keys[0]:
-            codigo.append(values[0])
+            codigo = codigo + str(values[0])
         elif letra in keys[1]:
-            codigo.append(values[1])
+            codigo = codigo + str(values[1])
         
     return codigo
 
 
-def codif(palabra):
-    codigo = []
+def codif(palabra, tree):
+    codigo = ''
     for letra in list(palabra):
-        codigo.append(codifletra(letra))
+        codigo = codigo + codifletra(letra, tree)
         
     return codigo
         
-def decod(codigo):
-    palabra = []
-    for i in list(codigo):
+def decod(codigo, tree):
+    palabra = ''
+    count = 0
+    for i in codigo:
+        nodo = np.flip(tree)[count]
+        keys = np.array(list(nodo.keys()))
+       
+        if len(keys[int(i)]) == 1:
+            palabra = palabra + keys[int(i)]
+            count = 0
+        else:
+            count = buscaSig(keys[int(i)], count, tree)
+    return palabra
         
+def buscaSig(key, count, tree):
+    for i in range(count+1, len(tree)):
+        nodo = np.flip(tree)[i]
+        keys = np.array(list(nodo.keys()))
+        if keys[0][0] in key:
+            return i
+        
+def huffmanS(tree, distr):
+    dic = []
+    L = 0
+    H = 0
+    for i in range(len(distr)):
+        c = distr['states'][i]
+        cod = codif(c, tree)
+        L = L + len(cod)*distr['probab'][i]
+        dic.append((c, cod))
+        H = H - distr['probab'][i]*np.log2(distr['probab'][i])
+    return dic, L, H
+
+
+def gini(distr):
+    GI = 0
+    for i in range(len(distr)):
+        GI = GI + distr['probab'][i]/len(distr)
+    return 1 - 2*GI
+
+
+def hill(distr):
+    hill = 0
+    for i in range(len(distr)):
+        hill = hill + distr['probab'][i]**2      
+    return 1/hill
+
 #Buscar cada estado dentro de cada uno de los dos items
 #tree[0].items()[0][0] ## Esto proporciona un '0'
 #tree[0].items()[1][0] ## Esto proporciona un '1'
-print(codifletra('f'))
-print(codif('fractal'))
+print(decod(codif('español', tree_es), tree_es))
+print(decod(codif('fractal', tree_en), tree_en))
+
+print('Codificacion de fractal en ingles: ' + codif('fractal', tree_en))
+print('con longitud: ' + str(len(codif('fractal', tree_en))))
+print('Codificacion de fractal en español: ' + codif('fractal', tree_es))
+print('con longitud: ' + str(len(codif('fractal', tree_es))))
+#Para cada caracter hacen falta x bits en funcion del  numero de estados que haya
+
+print('Arbol huffman ingles:')
+print(huffmanS(tree_en, distr_en))
+
+print('Arbol huffman español:')
+print(huffmanS(tree_es, distr_es))
+
+print(decod('0000111011111111111010', tree_en))
+
+print(gini(distr_en))
+print(hill(distr_en))
+
 plt.show()
