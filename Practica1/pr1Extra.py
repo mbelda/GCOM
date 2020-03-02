57895a6088ba606440cabda59992c398b460bc5c
Maria José Belda <majo.belda15@gmail.com>
2020-03-03 00:17:52 +0100

    - dim sierp

--- Practica1/pr1Extra.py
+++ Practica1/pr1Extra.py
@@ -14,21 +14,7 @@ blanco = np.array([255, 255, 255], dtype = np.uint8)
 d = 2
 for i in range(iteracionesTotales + 1): 
     tamCuadradoi = 3**(iteracionesTotales - i)
-    #Para cada iteración busco la medida de hausdorff
-    if i != 0:
-        #Delta cada iteracion se hace más pequeño por ser 1/3 < 1, así 
-        #simulamos el limite cuando tiende a 0
-        delta = (1/3)**i
-        #Tomamos la norma 1 (abs) de los recubrimientos (cuadrados)
-        norma = delta**2 
-        while True:
-            if  == 0
-            d =  
-        print('Medida it', i, ': ', d)
-        
-    else:
-        print('Log8/log3 =', np.log(8)/np.log(3))
-        
+    
     for x in range(3**i): 
         #Busco el cuadrado central
         if x % 3 == 1: 
@@ -42,7 +28,3 @@ for i in range(iteracionesTotales + 1):
 imgio.imwrite('sierpinski.jpg', cuadrado[:,:,0])
 im = Image.open("sierpinski.jpg") 
 im.show()
-
-
-    
-    
--- Practica1/pr1ExtraDim.py
+++ Practica1/pr1ExtraDim.py
@@ -4,24 +4,25 @@ Created on Wed Feb 26 12:33:47 2020
 
 @author: Majo
 """
-import sys
+import numpy as np
 
 iteraciones = 50
-epsilon = 1e-42
+epsilon = 1e-40
 
 def H(d):
     #lim delta -> 0
     suma = 0
-    for i in range(iteraciones):
-        delta = (1/3)**i
-        #suma normas 1 ^d
-        suma = 8**i * delta**2**d
+    i = iteraciones
+    delta = (1/3)**i
+    #suma normas 1 ^d
+    suma = 8**i * delta**2**d
     
     return suma
     
-d = 1.9
+d = 1.99
 while H(d) < epsilon :
     d = d - 1e-10
     print(d)
 
-print(H(2))
+print(d)
+print('Log8/log3 =', np.log(8)/np.log(3))
